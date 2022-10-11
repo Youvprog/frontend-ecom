d760830211051283d664f97c093a7989cf0cccf0
Youvprog <youminemyou@gmail.com>
2022-10-12 00:03:05 +0400

    api updated

--- src/service/ProductsService.js
+++ src/service/ProductsService.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apiCall = axios.create({
     //baseURL: 'https://youvprog.pythonanywhere.com/api/v1/',
-    baseURL: 'http://127.0.0.1:8000/api/v1/',
+    baseURL: 'https://youvprog.pythonanywhere.com/api/v1/',
 
 });
 
